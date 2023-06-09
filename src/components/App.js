eed15e9864beccbe4dcfa2d814a91c4374603942
AmyHMSmith <amyhmsmith@gmail.com>
2023-06-09 17:13:05 +0100

    location details rendered

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import LocationDetails from "./LocationDetails";
+
+function App({ location }) {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
+}
+
+export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders weather app', () => {
+test("renders weather app", () => {
   render(<App />);
   const linkElement = screen.getByText(/weather app/i);
   expect(linkElement).toBeInTheDocument();
