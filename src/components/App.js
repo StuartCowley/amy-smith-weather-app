d1a393bfb00293d01a94d3ff4aecdd4bca509b37
AmyHMSmith <amyhmsmith@gmail.com>
2023-06-11 16:06:27 +0100

    forecast summaries display (flexbox) rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+import "../styles/App.css";
 
 
 function App({ forecasts, location }) {
   return (
+    <div className="weather-app">
     <div className="forecast">
       <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries forecasts={forecasts} />
+    </div>
     </div>
   );
 };
